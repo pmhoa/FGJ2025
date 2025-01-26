3feaf6124d78f9c9df2bcd8eac6bc9a826eb86b7
Hoa Pham <60183197+pmhoa@users.noreply.github.com>
2025-01-26 16:15:41 +0200

    omat muutokset

--- Assets/Setämies69/scripts/StartGame.cs
+++ Assets/Setämies69/scripts/StartGame.cs
@@ -3,8 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+
+    public void DestroyAudioSources()
+    {
+        // Find all AudioSource components in the scene
+        AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+
+        // Loop through all AudioSource components and destroy them
+        foreach (AudioSource audioSource in audioSources)
+        {
+            Destroy(audioSource);
+        }
+    }
+
     public void OnButtonClick()
     {
+        DestroyAudioSources();
+
         // Load the next scene
         SceneManager.LoadScene(2);
     }
